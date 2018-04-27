bcb26271bffcedb76cb4cfec4beb3097751820b4
Devon <devonknudsen16@gmail.com>
2018-04-26 21:18:54 -0500

    Improvements to the last code include: changing grid class variable names, resizing window geometry, and changing the variables to be correct within create oval function.

--- Grid Trial.py
+++ Grid Trial.py
@@ -3,8 +3,12 @@ from Tkinter import *
 # class for the game grid
 class Grid(Canvas):
     POINT_RADIUS = 44
-    TOP_LEFT_X = 10
-    TOP_LEFT_Y = 10
+    
+    # column x's (need to find X for column 2, 3, 4, 5, 6, 7)
+    COLUMN1_X = 10
+    
+    # highest y before starting "piece" drop
+    TOP_Y = 10
     
     # need to define class variables such as midpoint values for grid
     def __init__(self, master):
@@ -19,10 +23,10 @@ class Grid(Canvas):
     # function used to draw the circles on the canvas
     # to simulate the placing of the pieces
     def placePiece(self):
-        self.create_oval(Grid.TOP_LEFT_X, Grid.TOP_LEFT_Y, Grid.TOP_LEFT_X + Grid.POINT_RADIUS * 2, Grid.TOP_LEFT_Y + Grid.POINT_RADIUS * 2, outline="black", fill="red")
+        self.create_oval(Grid.COLUMN1_X, Grid.TOP_Y, Grid.COLUMN1_X + Grid.POINT_RADIUS * 2, Grid.TOP_Y + Grid.POINT_RADIUS * 2, outline="black", fill="red")
 
 window = Tk()
-window.geometry("{}x{}".format(700, 800))
+window.geometry("{}x{}".format(710, 800))
 window.title("Connect 4")
 g = Grid(window)
 g.overlayGrid()
