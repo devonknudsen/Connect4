f9d6d12dc9ce562195777e0f31ca29c2b56e1523
Devon Knudsen <devonknudsen16@gmail.com>
2018-04-29 21:43:38 -0500

    Updated the Grid with things such as: x values above each column, top and mid y values, made the game full screen, made a setup gui function that overlays the grid, and creates 3 buttons including quit, settings and home, and I also updated the placepiece function to take in parameters. Once I change the worded buttons to image buttons, I will place this onto the main file and push the main file.

--- Grid Trial.py
+++ Grid Trial.py
@@ -4,31 +4,62 @@ from Tkinter import *
 class Grid(Canvas):
     POINT_RADIUS = 44
     
-    # column x's (need to find X for column 2, 3, 4, 5, 6, 7)
-    COLUMN1_X = 10
+    # highest x's for each column above the grid
+    COLUMN1_X = 305
+    COLUMN2_X = 405
+    COLUMN3_X = 505
+    COLUMN4_X = 605
+    COLUMN5_X = 705
+    COLUMN6_X = 805
+    COLUMN7_X = 905
     
     # highest y before starting "piece" drop
-    TOP_Y = 10
+    TOP_Y = 75
+    MID_Y = 170
     
     # need to define class variables such as midpoint values for grid
     def __init__(self, master):
         Canvas.__init__(self, master, bg="white")
         self.pack(fill=BOTH, expand=1)
+        master.attributes("-fullscreen", True)
         self.gameGridImg = PhotoImage(file="Connect4Grid.gif")
+        self.setupGUI()
+
+    # sets up GUI for game grid
+    def setupGUI(self):
+        # overlays grid image onto canvas
+        self.overlayGrid()
+
+        # creates a quit button in the top right corner
+        # needs to be made into an image button
+        quitButton = Button(self, text="Quit", command=window.destroy)
+        quitButton.pack(side=RIGHT, anchor=NE)
+
+        # creates a settings button in the top right corner
+        # needs to be made into an image button
+        settingsButton = Button(self, text="Settings")
+        settingsButton.pack(side=RIGHT, anchor=NE)
+
+        # creates a home button in the top right corner
+        # needs to be made into an image button
+        homeButton = Button(self, text="Home")
+        homeButton.pack(side=RIGHT, anchor=NE)
+        
 
     # function used to overlay the grid on top of the canvas 
     def overlayGrid(self):
-        self.create_image(5, 0, image=self.gameGridImg, anchor=NW)
+        self.create_image(300, 65, image=self.gameGridImg, anchor=NW)
 
     # function used to draw the circles on the canvas
     # to simulate the placing of the pieces
-    def placePiece(self):
-        self.create_oval(Grid.COLUMN1_X, Grid.TOP_Y, Grid.COLUMN1_X + Grid.POINT_RADIUS * 2, Grid.TOP_Y + Grid.POINT_RADIUS * 2, outline="black", fill="red")
+    def placePiece(self, x, y, player):
+        if player == "p1":
+            self.create_oval(x, y, x + Piece.pieceRadius * 2, y + Piece.pieceRadius * 2, outline="black", fill=Piece.plColor)
+        if player == "p2":
+            self.create_oval(x, y, x + Piece.pieceRadius * 2, y + Piece.pieceRadius * 2, outline="black", fill=Piece.p2Color)
 
 window = Tk()
-window.geometry("{}x{}".format(710, 800))
 window.title("Connect 4")
 g = Grid(window)
-g.overlayGrid()
 g.placePiece()
 window.mainloop()
