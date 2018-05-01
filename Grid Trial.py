82eacd550fecfa750ae213e3d5208f4a314d4e6d
Devon <devonknudsen16@gmail.com>
2018-04-30 21:24:25 -0500

    The Pi optimized version of the grid program for the connect 4 game. I also added the home and exit buttons.

--- Grid Trial.py
+++ Grid Trial.py
@@ -2,27 +2,28 @@ from Tkinter import *
 
 # class for the game grid
 class Grid(Canvas):
-    POINT_RADIUS = 44
+    POINT_RADIUS = 28
     
     # highest x's for each column above the grid
-    COLUMN1_X = 305
-    COLUMN2_X = 405
-    COLUMN3_X = 505
-    COLUMN4_X = 605
-    COLUMN5_X = 705
-    COLUMN6_X = 805
-    COLUMN7_X = 905
+    # differnence between slots is 65
+    COLUMN1_X = 154
+    COLUMN2_X = 219
+    COLUMN3_X = 284
+    COLUMN4_X = 349
+    COLUMN5_X = 414
+    COLUMN6_X = 479
+    COLUMN7_X = 544
     
     # highest y before starting "piece" drop
-    TOP_Y = 75
-    MID_Y = 170
+    TOP_Y = 32
+    MID_Y = 94
     
     # need to define class variables such as midpoint values for grid
     def __init__(self, master):
         Canvas.__init__(self, master, bg="white")
         self.pack(fill=BOTH, expand=1)
         master.attributes("-fullscreen", True)
-        self.gameGridImg = PhotoImage(file="Connect4Grid.gif")
+        self.gameGridImg = PhotoImage(file="Connect4GridPi.gif")
         self.setupGUI()
 
     # sets up GUI for game grid
@@ -31,35 +32,32 @@ class Grid(Canvas):
         self.overlayGrid()
 
         # creates a quit button in the top right corner
-        # needs to be made into an image button
-        quitButton = Button(self, text="Quit", command=window.destroy)
-        quitButton.pack(side=RIGHT, anchor=NE)
-
-        # creates a settings button in the top right corner
-        # needs to be made into an image button
-        settingsButton = Button(self, text="Settings")
-        settingsButton.pack(side=RIGHT, anchor=NE)
-
+        redXimg = PhotoImage(file="RedXPi.gif")
+        redXButton = Button(self, image=redXimg, command=window.destroy)
+        redXButton.configure(background="white")
+        redXButton.image = redXimg
+        redXButton.pack(side=RIGHT, anchor=NE)
+        
         # creates a home button in the top right corner
-        # needs to be made into an image button
-        homeButton = Button(self, text="Home")
+        homeButtonImg = PhotoImage(file="HomeButtonPi.gif")
+        homeButton = Button(self, image=homeButtonImg)
+        homeButton.configure(background="white")
+        homeButton.image = homeButtonImg
         homeButton.pack(side=RIGHT, anchor=NE)
         
 
     # function used to overlay the grid on top of the canvas 
     def overlayGrid(self):
-        self.create_image(300, 65, image=self.gameGridImg, anchor=NW)
+        self.create_image(150, 25, image=self.gameGridImg, anchor=NW)
 
     # function used to draw the circles on the canvas
     # to simulate the placing of the pieces
-    def placePiece(self, x, y, player):
-        if player == "p1":
-            self.create_oval(x, y, x + Piece.pieceRadius * 2, y + Piece.pieceRadius * 2, outline="black", fill=Piece.plColor)
-        if player == "p2":
-            self.create_oval(x, y, x + Piece.pieceRadius * 2, y + Piece.pieceRadius * 2, outline="black", fill=Piece.p2Color)
+    # must change radius back to point radius and colors back to strs before implemeting into main game file
+    def placePiece(self):
+            self.create_oval(Grid.COLUMN7_X , Grid.MID_Y, Grid.COLUMN7_X  + Grid.POINT_RADIUS * 2, Grid.MID_Y + Grid.POINT_RADIUS * 2, outline="black", fill="red")
+        
 
 window = Tk()
 window.title("Connect 4")
 g = Grid(window)
-g.placePiece()
 window.mainloop()
